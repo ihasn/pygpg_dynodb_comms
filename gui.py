9a69d5b64595276862a06084a883805e02227a77
ihasn <ihasn2004@gmail.com>
2014-08-06 22:10:34 -0400

    initial gui for app setup

--- gui.py
+++ gui.py
@@ -1,1 +1,58 @@
-#This will be the GUI for pygpg_dynodb_comms ui.
+#!/usr/bin/python
+# -*- coding: iso-8859-1 -*-
+
+import Tkinter
+#Import gnupg, getpass, urlib
+import gnupg, getpass, urllib
+#Import time stuff
+from time import gmtime, strftime
+
+
+class simpleapp_tk(Tkinter.Tk):
+    def __init__(self,parent):
+        Tkinter.Tk.__init__(self,parent)
+        self.parent = parent
+        self.initialize()
+
+    def initialize(self):
+        self.grid()
+
+        self.entryVariable = Tkinter.StringVar()
+        self.entry = Tkinter.Entry(self,textvariable=self.entryVariable)
+        self.entry.grid(column=0,row=0,sticky='EW')
+        self.entry.bind("<Return>", self.OnPressEnter)
+        self.entryVariable.set(u"Enter text here.")
+
+        button = Tkinter.Button(self,text=u"Click me !",
+                                command=self.OnButtonClick)
+        button.grid(column=1,row=0)
+
+        self.labelVariable = Tkinter.StringVar()
+        label = Tkinter.Label(self,textvariable=self.labelVariable,
+                              anchor="w",fg="white",bg="blue")
+        label.grid(column=0,row=1,columnspan=2,sticky='EW')
+        self.labelVariable.set(u"Hello !")
+
+        self.grid_columnconfigure(0,weight=1)
+        self.resizable(True,False)
+        self.update()
+        self.geometry(self.geometry())       
+        self.entry.focus_set()
+        self.entry.selection_range(0, Tkinter.END)
+
+    def OnButtonClick(self):
+        self.entry.focus_set()
+        gpg = gnupg.GPG(binary='/usr/bin/gpg2', homedir='/home/izen/.gnupg')
+        public_keys = gpg.list_keys()
+        self.labelVariable.set( self.entryVariable.get()+ public_keys.fingerprints[0] )
+        self.entry.selection_range(0, Tkinter.END)
+
+    def OnPressEnter(self,event):
+        self.labelVariable.set( self.entryVariable.get()+" (You pressed ENTER)" )
+        self.entry.focus_set()
+        self.entry.selection_range(0, Tkinter.END)
+
+if __name__ == "__main__":
+    app = simpleapp_tk(None)
+    app.title('GPG DynamoDB Comms')
+    app.mainloop()
